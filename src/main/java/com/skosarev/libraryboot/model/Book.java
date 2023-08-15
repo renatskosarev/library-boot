a6cfff42b483224fd93454ca77fb0b7b0f6570e8
Renat Skosarev <skosarrrev@gmail.com>
2023-08-15 20:44:21 +0500

    Migrate to Java 17 & Spring Boot 3

--- src/main/java/com/skosarev/libraryboot/model/Book.java
+++ src/main/java/com/skosarev/libraryboot/model/Book.java
@@ -2,11 +2,11 @@ package com.skosarev.libraryboot.model;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.persistence.*;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
--- src/main/java/com/skosarev/libraryboot/model/Person.java
+++ src/main/java/com/skosarev/libraryboot/model/Person.java
@@ -1,10 +1,10 @@
 package com.skosarev.libraryboot.model;
 
-import javax.persistence.*;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.util.List;
 
 @Entity
